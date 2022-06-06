7a575384a9b9141ddec32053bdfee29f64f7b267
patil1001970 <patil1001970@gmail.com>
2022-06-06 21:42:04 +0530

    1 file modified, 1 added

--- demo.js
+++ demo.js
@@ -3,12 +3,10 @@ import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Mycard from './mycard';
-
+import Masonry from '@mui/lab/Masonry';
 import MyJson from './mysample.json';
 import Grid from '@mui/material/Grid';
-
-
-
+import Navbar from './navBar';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -18,22 +16,24 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function FixedColumns() {
+  const updateNews = async () => {
 
+  };
 
-  const updateNews= async ()=>{
-
-  }
-
-  const {articles}=MyJson;
+  const { articles } = MyJson;
   return (
-    <Box sx={{  minHeight: 253 }}>
-    <Grid container sx={{p:5}} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+    <>
+    <Navbar/>
+    <Box sx={{ minHeight: 253 }}>
+      <Masonry columns={{ xs: 1, sm: 3 }} spacing={0.5}>
         {articles.map((height, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
-          <Mycard key={index} article={height}/>
+          <Grid item xs={2} sm={4} md={4} key={index} sx={{ p: 2 }}>
+            <Mycard key={index} article={height} />
           </Grid>
         ))}
-      </Grid>
+      </Masonry>
     </Box>
+
+    </>
   );
 }
